3d6b8848a0d2d7b7bda1081f1b328d1fa1137680
Sandeep Sachan <sachan.sandeep@gmail.com>
2021-08-19 11:53:56 +0530

    Update CategoryControllerTests.cs

--- test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
+++ test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -10,30 +10,5 @@ using Xunit;
 namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers
 {
 
-    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
-    {
-        private readonly CustomWebApplicationFactory<Startup> _factory;
-
-        public CategoryControllerTests(CustomWebApplicationFactory<Startup> factory)
-        {
-            _factory = factory;
-        }
-
-        [Fact]
-        public async Task ReturnsSuccessResult()
-        {
-            var client = _factory.GetAnonymousClient();
-
-           // var response = await client.GetAsync("/api/V1/category/all");
-
-           // response.EnsureSuccessStatusCode();
-
-           // var responseString = await response.Content.ReadAsStringAsync();
-
-           // var result = JsonConvert.DeserializeObject<Response<IEnumerable<CategoryListVm>>>(responseString);
-            
-            //Assert.IsType<List<CategoryListVm>>(result.Data);
-           // Assert.NotEmpty(result.Data);
-        }
-    }
+    
 }
