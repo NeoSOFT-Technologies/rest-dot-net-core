668d83b9edae59168a91dd11c5847a756f27d8c1
Neosoft <Neosoft@DESKTOP-44DAAQS>
2021-08-31 10:18:31 +0530

    Added unit test cases.

---
+++ test/GloboTicket.TicketManagement.Application.UnitTests/Events/Queries/GetEventListQueryHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.Responses;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Event.Queries
+{
+    public class GetEventListQueryHandlerTests
+    {
+        private readonly IMapper _mapper;
+        //private readonly Mock<IAsyncRepository<GloboTicket.TicketManagement.Domain.Entities.Event>> _mockEventRepository;
+        private readonly Mock<IEventRepository> _mockEventRepository;
+        private readonly ILogger<GetEventsListQueryHandler> _logger;
+        //private readonly ILogger<CreateEventCommandHandler> _logger;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public GetEventListQueryHandlerTests()
+        {
+            _mockEventRepository = RepositoryMocksEvent.GetEventRepository();
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            //_logger = new Mock<ILogger<GetEventListQueryHandler>>();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+       
+        [Fact]
+        public async Task GetEventListTest()
+        {
+            var handler = new GetEventDetailQueryHandler(_mapper, _mockEventRepository.Object, _mockCategoryRepository.Object);
+
+            var eventdetail = new GetEventDetailQuery();
+            eventdetail.Id = new Guid("B0788D2F-8003-43C1-92A4-EDC76A7C5DDE");
+            _mockEventRepository.Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(new GloboTicket.TicketManagement.Domain.Entities.Event
+            {
+                EventId = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}"),
+                Name = "Concerts"
+            });
+            _mockCategoryRepository.Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(new Category
+            {
+                CategoryId = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}"),
+                Name = "Concerts"
+            });
+            var result = await handler.Handle(eventdetail, CancellationToken.None);
+
+            result.ShouldBeOfType<Response<EventDetailVm>>();
+
+        }
+    }
+}
---
+++ test/GloboTicket.TicketManagement.Application.UnitTests/Mocks/RepositoryMocksEvent.cs
@@ -0,0 +1,61 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Mocks
+{
+    public class RepositoryMocksEvent
+    {
+        public static Mock<IEventRepository> GetEventRepository()
+        {
+            
+            var EventList = new List<GloboTicket.TicketManagement.Domain.Entities.Event>
+            {
+                new Domain.Entities.Event
+                {
+                    EventId = Guid.Parse("ADC42C09-08C1-4D2C-9F96-2D15BB1AF299"),
+                    //Name = "To the Moon and Back",
+                    //Price=25,
+                    //Artist = "Nick Sailor",
+                    //Date=DateTime.Now,
+                    //Description = "The critics are over the moon and so will you after you've watched this sing and dance extravaganza written by Nick Sailor, the man from 'My dad and sister'.",
+                    //ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/musical.jpg",
+                    //CategoryId = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}")
+                Name = "Test",
+                Price = 25,
+                Artist = "test",
+                Date = new DateTime(2027, 1, 18),
+                Description = "description",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/musical.jpg",
+                CategoryId = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}")
+                }
+                //new Domain.Entities.Event
+                //{
+                //    EventId = Guid.Parse("{B419A7CA-3321-4F38-BE8E-4D7B6A529319}"),
+                //    Name = "Clash of the DJs",
+                //    Artist = "DJ 'The Mike'",
+                //    Description = "DJs from all over the world will compete in this epic battle for eternal fame.",
+                //    ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/musical.jpg",
+                //    CategoryId = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}")
+                //}
+
+            };
+
+            var mockEventRepository = new Mock<IEventRepository>();
+            mockEventRepository.Setup(repo => repo.ListAllAsync()).ReturnsAsync(EventList);
+            mockEventRepository.Setup(repo => repo.GetByIdAsync(Guid.Parse("ADC42C09-08C1-4D2C-9F96-2D15BB1AF299")));
+
+            mockEventRepository.Setup(repo => repo.AddAsync(It.IsAny<GloboTicket.TicketManagement.Domain.Entities.Event>())).ReturnsAsync(
+                (GloboTicket.TicketManagement.Domain.Entities.Event Event) =>
+                {
+                    EventList.Add(Event);
+                    return Event;
+                });
+
+            return mockEventRepository;
+
+        }
+    }
+}
