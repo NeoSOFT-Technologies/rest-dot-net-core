f83c5f00c5a170c75b242d01d68c32eb2886219a
apoorv-rane2 <apoorv.rane@neosoftmail.com>
2021-09-02 15:16:00 +0530

    Added comment

--- src/API/GloboTicket.TicketManagement.Api/Controllers/v2/CategoryController.cs
+++ src/API/GloboTicket.TicketManagement.Api/Controllers/v2/CategoryController.cs
@@ -54,6 +54,7 @@ namespace GloboTicket.TicketManagement.Api.Controllers.v2
         {
             var response = await _mediator.Send(createCategoryCommand);
             return Ok(response);
+            //test
         }
     }
 }
