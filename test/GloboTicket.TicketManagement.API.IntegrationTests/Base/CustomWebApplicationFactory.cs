df7ac4489bf350e110cae2f0161d619ecfa80732
shreedhar-ww <shreedhar.deshwal@gmail.com>
2021-09-06 08:56:00 +0530

    CI build

--- test/GloboTicket.TicketManagement.API.IntegrationTests/Base/CustomWebApplicationFactory.cs
+++ test/GloboTicket.TicketManagement.API.IntegrationTests/Base/CustomWebApplicationFactory.cs
@@ -1,47 +1,66 @@
-﻿using GloboTicket.TicketManagement.Persistence;
+﻿using GloboTicket.TicketManagement.Api;
+using GloboTicket.TicketManagement.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace GloboTicket.TicketManagement.API.IntegrationTests.Base
 {
-    public class CustomWebApplicationFactory<TStartup>
-            : WebApplicationFactory<TStartup> where TStartup : class
+    public class CustomWebApplicationFactory
+            : WebApplicationFactory<Startup>
     {
+
+        private readonly DbFixture _dbFixture;
+
+        public CustomWebApplicationFactory()
+        { _dbFixture = new DbFixture(); }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.ConfigureServices(services =>
+
+            builder.ConfigureAppConfiguration((context, config) =>
             {
-
-                services.AddDbContext<GloboTicketDbContext>(options =>
+                config.AddInMemoryCollection(new[]
                 {
-                    options.UseInMemoryDatabase("GloboTicketDbContextInMemoryTest");
+                    new KeyValuePair<string, string>(
+                        "ConnectionStrings:BlogConnection", _dbFixture.ConnString)
                 });
-
-                var sp = services.BuildServiceProvider();
-
-                using (var scope = sp.CreateScope())
-                {
-                    var scopedServices = scope.ServiceProvider;
-                    var context = scopedServices.GetRequiredService<GloboTicketDbContext>();
-                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
-
-                    context.Database.EnsureCreated();
-
-                    try
-                    {
-                        Utilities.InitializeDbForTests(context);
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, $"An error occurred seeding the database with test messages. Error: {ex.Message}");
-                    }
-                };
             });
+
+            //builder.ConfigureServices(services =>
+            //{
+
+            //    services.AddDbContext<GloboTicketDbContext>(options =>
+            //    {
+            //        options.UseInMemoryDatabase("GloboTicketDbContextInMemoryTest");
+            //    });
+
+            //    var sp = services.BuildServiceProvider();
+
+            //    using (var scope = sp.CreateScope())
+            //    {
+            //        var scopedServices = scope.ServiceProvider;
+            //        var context = scopedServices.GetRequiredService<GloboTicketDbContext>();
+            //        var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+            //        context.Database.EnsureCreated();
+
+            //        try
+            //        {
+            //            Utilities.InitializeDbForTests(context);
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            logger.LogError(ex, $"An error occurred seeding the database with test messages. Error: {ex.Message}");
+            //        }
+            //    };
+            //});
         }
 
         public HttpClient GetAnonymousClient()
---
+++ test/GloboTicket.TicketManagement.API.IntegrationTests/Base/DbFixture.cs
@@ -0,0 +1,62 @@
+﻿using GloboTicket.TicketManagement.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.API.IntegrationTests.Base
+{
+    /// <summary>
+    /// A collection fixture that is responsible for creating and dropping the database
+    /// https://xunit.net/docs/shared-context
+    /// </summary>
+    public class DbFixture : IDisposable
+    {
+        private readonly GloboTicketDbContext _dbContext;
+        public readonly string BlogDbName = $"Blog-{Guid.NewGuid()}";
+        public readonly string ConnString;
+
+        private bool _disposed;
+
+        public DbFixture()
+        {
+            ConnString = $"Server=localhost,1433;Database={BlogDbName};User=sa;Password=2@LaiNw)PDvs^t>L!Ybt]6H^%h3U>M";
+
+            var builder = new DbContextOptionsBuilder<GloboTicketDbContext>();
+
+            builder.UseSqlServer(ConnString);
+            _dbContext = new GloboTicketDbContext(builder.Options);
+
+            _dbContext.Database.Migrate();
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // remove the temp db from the server once all tests are done
+                    _dbContext.Database.EnsureDeleted();
+                }
+
+                _disposed = true;
+            }
+        }
+    }
+
+    [CollectionDefinition("Database")]
+    public class DatabaseCollection : ICollectionFixture<DbFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
