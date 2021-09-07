7cb8ad12f8a7e0ce2eaad263ba079d1edfa48279
Neosoft <Neosoft@DESKTOP-44DAAQS>
2021-09-07 20:37:52 +0530

    Removed white spaces.

--- src/API/GloboTicket.TicketManagement.Api/Startup.cs
+++ src/API/GloboTicket.TicketManagement.Api/Startup.cs
@@ -75,9 +75,7 @@ namespace GloboTicket.TicketManagement.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseAuthentication();
-
-           
+            app.UseAuthentication();          
 
             app.UseSwagger();
 
@@ -92,7 +90,6 @@ namespace GloboTicket.TicketManagement.Api
                     options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
                 }
             });
-
 
             app.UseCustomExceptionHandler();
 
@@ -118,10 +115,7 @@ namespace GloboTicket.TicketManagement.Api
                 endpoints.MapHealthChecksUI();
 
             });           
-        }
-
-
-       
+        }       
 
     }
 
