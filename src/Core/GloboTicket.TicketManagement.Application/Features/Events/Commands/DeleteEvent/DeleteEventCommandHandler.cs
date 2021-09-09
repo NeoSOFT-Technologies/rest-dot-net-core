a1d3c4b6ce34ad948d28dc6a12fd6385abd14762
apoorv-rane2 <apoorv.rane@neosoftmail.com>
2021-09-09 12:42:58 +0530

    Unit test fixes and put,delete event integration tests added

--- src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -4,6 +4,8 @@ using GloboTicket.TicketManagement.Application.Exceptions;
 using GloboTicket.TicketManagement.Application.Responses;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.DataProtection;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,20 +13,23 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Dele
 {
     public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
     {
-        private readonly IEventRepository _eventRepository;         
-        
-        public DeleteEventCommandHandler(IEventRepository eventRepository)
+        private readonly IEventRepository _eventRepository;
+        private readonly IDataProtector _protector;
+
+        public DeleteEventCommandHandler(IEventRepository eventRepository, IDataProtectionProvider provider)
         {             
             _eventRepository = eventRepository;
+            _protector = provider.CreateProtector("");
         }
 
         public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
         {
-            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+            var eventId = new Guid(_protector.Unprotect(request.EventId));
+            var eventToDelete = await _eventRepository.GetByIdAsync(eventId);
 
             if (eventToDelete == null)
             {
-                throw new NotFoundException(nameof(Event), request.EventId);
+                throw new NotFoundException(nameof(Event), eventId);
             }
 
             await _eventRepository.DeleteAsync(eventToDelete);           
--- test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/v1/EventControllerTests.cs
+++ test/GloboTicket.TicketManagement.API.IntegrationTests/Controllers/v1/EventControllerTests.cs
@@ -1,11 +1,9 @@
-﻿using GloboTicket.TicketManagement.Api;
-using GloboTicket.TicketManagement.API.IntegrationTests.Base;
+﻿using GloboTicket.TicketManagement.API.IntegrationTests.Base;
 using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Application.Responses;
-using GloboTicket.TicketManagement.Domain.Entities;
 using Newtonsoft.Json;
 using Shouldly;
 using System;
@@ -15,7 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc.Testing;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
 
 namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers.v1
 {
@@ -41,14 +39,11 @@ namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers.v1
             result.Data.ShouldNotBeEmpty();
         }
 
-       [Fact]
+        [Fact]
         public async Task Get_EventDetail_ReturnsSuccessResult()
         {
             var client = _factory.CreateClient();
-            var eventList = await client.GetAsync("/api/V1/events");
-            var eventListString = await eventList.Content.ReadAsStringAsync();
-            var eventListResult = JsonConvert.DeserializeObject<Response<IEnumerable<EventListVm>>>(eventListString);
-            var eventId = eventListResult.Data.FirstOrDefault().EventId;
+            var eventId = await GetFirstEventId();
             var response = await client.GetAsync("/api/V1/events/" + eventId);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
@@ -98,6 +93,50 @@ namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers.v1
             result.Succeeded.ShouldBeEquivalentTo(true);
             result.Data.ShouldBeOfType<Guid>();
             result.Errors.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Put_Event_ReturnsSuccessResult()
+        {
+            var client = _factory.CreateClient();
+            var @event = new UpdateEventCommand()
+            {
+                EventId = Guid.Parse("{3448D5A4-0F72-4DD7-BF15-C14A46B26C00}"),
+                Name = "Test Name1",
+                Price = 75,
+                Artist = "Test Artist",
+                Date = DateTime.Now.AddMonths(6),
+                Description = "Test Description",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/banjo.jpg",
+                CategoryId = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}")
+            };
+            var eventJson = JsonConvert.SerializeObject(@event);
+            HttpContent content = new StringContent(eventJson, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("/api/v1/events", content);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Response<Guid>>(responseString);
+            result.Succeeded.ShouldBeEquivalentTo(true);
+            result.Data.ShouldBeOfType<Guid>();
+            result.Errors.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Delete_Event_ReturnsSuccessResult()
+        {
+            var client = _factory.CreateClient();
+            var eventId = await GetFirstEventId();
+            var response = await client.DeleteAsync("/api/V1/events/" + eventId);
+            response.EnsureSuccessStatusCode();
+        }
+
+        private async Task<string> GetFirstEventId()
+        {
+            var client = _factory.CreateClient();
+            var eventList = await client.GetAsync("/api/V1/events");
+            var eventListString = await eventList.Content.ReadAsStringAsync();
+            var eventListResult = JsonConvert.DeserializeObject<Response<IEnumerable<EventListVm>>>(eventListString);
+            return eventListResult.Data.FirstOrDefault().EventId;
         }
     }
 }
