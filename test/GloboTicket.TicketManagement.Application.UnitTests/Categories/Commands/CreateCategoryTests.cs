bee52f71e3bcd64542cf02f698eeffa4d3d38005
apoorv-rane2 <apoorv.rane@neosoftmail.com>
2021-09-22 10:45:34 +0530

    Updated assert of CreateCategoryTests

--- test/GloboTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
+++ test/GloboTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -2,8 +2,8 @@
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
 using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.Responses;
 using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
-using GloboTicket.TicketManagement.Domain.Entities;
 using Moq;
 using Shouldly;
 using System.Threading;
@@ -33,9 +33,13 @@ namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Commands
         {
             var handler = new CreateCategoryCommandHandler(_mapper, _mockCategoryRepository.Object);
 
-            await handler.Handle(new CreateCategoryCommand() { Name = "Test" }, CancellationToken.None);
+            var result = await handler.Handle(new CreateCategoryCommand() { Name = "Test" }, CancellationToken.None);
 
             var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
+
+            result.ShouldBeOfType<Response<CreateCategoryDto>>();
+            result.Succeeded.ShouldBe(true);
+            result.Errors.ShouldBeNull();
             allCategories.Count.ShouldBe(5);
         }
 
@@ -48,7 +52,11 @@ namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Commands
 
             var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
 
-            Assert.Equal("name is required.", result.Errors[0].ToLower());
+            result.ShouldBeOfType<Response<CreateCategoryDto>>();
+            result.Succeeded.ShouldBe(false);
+            result.Errors.ShouldNotBeNull();
+            result.Errors[0].ToLower().ShouldBe("name is required.");
+            allCategories.Count.ShouldBe(4);
         }
 
         [Fact]
@@ -60,7 +68,11 @@ namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Commands
 
             var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
 
-            Assert.Equal("name must not exceed 10 characters.", result.Errors[0].ToLower());
+            result.ShouldBeOfType<Response<CreateCategoryDto>>();
+            result.Succeeded.ShouldBe(false);
+            result.Errors.ShouldNotBeNull();
+            result.Errors[0].ToLower().ShouldBe("name must not exceed 10 characters.");
+            allCategories.Count.ShouldBe(4);
         }
     }
 }
