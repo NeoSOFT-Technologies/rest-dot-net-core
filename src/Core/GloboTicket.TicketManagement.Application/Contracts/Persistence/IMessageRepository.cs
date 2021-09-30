7c58405cf530a886608a1380fc5259c33c3446a0
Neosoft <Neosoft@DESKTOP-44DAAQS>
2021-09-30 18:04:55 +0530

    Added Message repository.

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IMessageRepository : IAsyncRepository<Notification>
+    {
+        public Task<List<Notification>> GetAllNotifications();
+
+
+    }
+}
---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/MessageRepository.cs
@@ -0,0 +1,33 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Notifications;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class MessageRepository : BaseRepository<Notificationss>, IMessageRepository
+    {
+
+        private readonly ILogger _logger;
+        public MessageRepository(GloboTicketDbContext dbContext, ILogger<Notificationss> logger) : base(dbContext, logger)
+        {
+            _logger = logger;
+        }
+
+        public  Task<List<Notificationss>> GetAllNotifications()
+        {
+
+           var Allnotifications =  _dbContext.NotificationMaster.Include(x => x.NotificationCode).ToListAsync();
+            return Allnotifications;
+
+          
+        }
+
+      
+        
+
+    }
+}
